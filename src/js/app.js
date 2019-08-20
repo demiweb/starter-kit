5a247b2c1f3b69b49aa925f3ecd6990b2384fd5f
yurayarosh <yura12323@gmail.com>
2019-08-20 19:44:19 +0300

    add closest polyfill

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
-import '@babel/polyfill';
 import $ from 'jquery';
+// import '@babel/polyfill';
+import './lib/polyfill';
 import sayHello from './lib/sayHello';
 import setTouch from './components/setHTMLClassNames';
 import setLazy from './components/setLazy';
