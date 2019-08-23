f6707c3cd379e4ad48874fe9bc7cdc833014ec9c
yurayarosh <yura12323@gmail.com>
2019-08-23 22:05:12 +0300

    make setHTMLClassNames() as one function, add setReady(), upd include fonts, common styles, main layout

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,11 @@ import $ from 'jquery';
 // import '@babel/polyfill';
 import './lib/polyfill';
 import sayHello from './lib/sayHello';
-import setTouch from './components/setHTMLClassNames';
+import setHTMLClassNames from './components/setHTMLClassNames';
 import setLazy from './components/setLazy';
 
 $(() => {
   sayHello();
-  setTouch();
+  setHTMLClassNames();
   setLazy();
 });
--- src/js/components/setHTMLClassNames.js
+++ src/js/components/setHTMLClassNames.js
@@ -1,8 +1,17 @@
 import { isTouch } from '../helpers';
-import { NO_TOUCH } from '../constants';
+import { NO_TOUCH, IS_READY } from '../constants';
 
-export default function setTouch() {
+function setTouch() {
   if (!isTouch) {
     document.documentElement.classList.add(NO_TOUCH);
   }
+}
+
+function setReady() {
+  document.documentElement.classList.add(IS_READY);
+}
+
+export default function setHTMLClassNames() {
+  setTouch();
+  setReady();
 }
